ea9c52b4cb04f4d20b9b56b830e9d19c0ad7b032
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-09-30 17:28:24 +0300

    api call making request to openweather api

--- src/controllers/weather.js
+++ src/controllers/weather.js
@@ -1,5 +1,24 @@
-const Weather = require('../models/weather')
+// const Weather = require('../models/weather')
+const rp = require('request-promise')
+const { removeEmptyFromObject } = require('../utils/common')
 
 module.exports.getWeather = async (req, res) => {
-  res.status(200).send()
+  const { q, lat, lng, id } = req.query
+  const qs = removeEmptyFromObject({
+    q,
+    lat,
+    lng,
+    id,
+    appid: process.env.WEATHER_API_KEY
+  })
+  const options = {
+    uri: process.env.WEATHER_API_URL + 'forecast',
+    qs,
+    json: true
+  }
+
+  const data = await rp(options)
+  if (data.cod !== '200') return res.status(400).send()
+
+  res.status(200).send(data)
 }
--- src/utils/common.js
+++ src/utils/common.js
@@ -2,3 +2,9 @@ module.exports.asyncMiddleware = fn => (req, res, next) => {
   Promise.resolve(fn(req, res, next))
   .catch(next)
 }
+
+module.exports.removeEmptyFromObject = obj => {
+  Object.keys(obj)
+    .forEach(key => obj[key] == null && delete obj[key])
+  return obj
+}
